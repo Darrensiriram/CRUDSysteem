a3985f4213d30993c2f6643d1f69c8860610537c
darrensiriram <ds.siriram@gmail.com>
2019-06-03 09:38:44 +0200

    1e versie werkende database

---
+++ src/projectfullhouse/ProjectFullhouse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectfullhouse;
+
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author darrensiriram
+ */
+public class ProjectFullhouse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BeginScherm bs = new BeginScherm();
+        bs.setVisible(true);
+        
+    }
+    
+}
